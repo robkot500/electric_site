24e4cab6df5a9df573bfe586fd2d7d36638483f6
robkot500 <robert.kotwicki5@gmail.com>
2020-08-13 11:49:00 +0200

    Updates

---
+++ precache-manifest.71ca002764216a56860332792d84de39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd920866d60e882d1a424b623b53e64",
+    "url": "/electric_site/index.html"
+  },
+  {
+    "revision": "773c62bc7d4e50678df6",
+    "url": "/electric_site/static/css/main.0584b685.chunk.css"
+  },
+  {
+    "revision": "76822031a593c1125d26",
+    "url": "/electric_site/static/js/2.26f6cc70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/electric_site/static/js/2.26f6cc70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773c62bc7d4e50678df6",
+    "url": "/electric_site/static/js/main.903930e2.chunk.js"
+  },
+  {
+    "revision": "426706ab87c3c1a220ef",
+    "url": "/electric_site/static/js/runtime-main.67f05231.js"
+  }
+]);
