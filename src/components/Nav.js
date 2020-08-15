e60b2c6e9bddf0d28dcf81ea34023b90deda6351
robkot500 <robert.kotwicki5@gmail.com>
2020-08-15 12:50:20 +0200

    version blue

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 function Nav() {
     return (
         <div className='nav-container'>
-            <motion.div animate={{ height: 40, x: 150, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }} className='logo'>
-            </motion.div>
+            {/* <motion.div animate={{ height: 40, x: 150, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }} className='logo'>
+            </motion.div> */}
             <nav>
                 <ul>
                     <li>STRONA GŁÓWNA</li>
