10921c664250cd4ed81e87a1e28026faf3dc4c90
robkot500 <robert.kotwicki5@gmail.com>
2020-08-17 08:18:54 +0200

    useInView

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+// import { useState, useEffect } from 'react';
+// import UseAnimations from 'react-useanimations';
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 function Main() {
+
+    const [ref, inView] = useInView({
+        rootMargin: '-100px 0px',
+    })
+    const controls = useAnimation();
+
+    controls.start({
+        y: -120, opacity: 1
+    })
     return (
         <div className='main'>
-            <motion.div className='main-slogan' animate={{ y: -120, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }} >Profesjonalne usługi elektryczne w Rybniku i okolicach. Zaufaj wiedzy i doświadczeniu.</motion.div>
-            <motion.div className='main-item-wrapper' animate={{ y: -120, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }}>
+            {/* <motion.div className='main-slogan' animate={{ y: -120, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }} >Profesjonalne usługi elektryczne w Rybniku i okolicach. Zaufaj wiedzy i doświadczeniu.</motion.div> */}
+            <motion.div className='main-slogan' animate={controls} transition={{ ease: "easeOut", duration: 1 }} >Profesjonalne usługi elektryczne w Rybniku i okolicach. Zaufaj wiedzy i doświadczeniu.</motion.div>
+
+            <motion.div className='main-item-wrapper' ref={ref} animate={inView ? (console.log('hhhhhhhh'), { y: -120, opacity: 1 }) : ({})} transition={{ ease: "easeOut", duration: 0.5 }}>
                 <div className="main-item">
                     <div class="icon-wrapper"><i class="fas fa-user-cog"></i></div>
                     <div class="main-item-slogan"> Wiedza i doświadczenie</div>
@@ -23,7 +37,7 @@ function Main() {
                     <div class="main-item-slogan">Proste zamówienie</div>
                 </div>
             </motion.div>
-        </div>
+        </div >
     )
 }
 
