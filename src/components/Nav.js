837aebb4e729f3deb42e54aa1749c99f50c1ff94
robkot500 <robert.kotwicki5@gmail.com>
2020-08-22 19:23:47 +0200

    hamburger

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
+import { Link } from "react-scroll";
 
 function Nav() {
+
+    const [hamburger, setHamburger] = useState(false)
+    const handleClick = () => {
+        setHamburger(!hamburger)
+    }
     return (
-        <div className='nav-container'>
-            <motion.div animate={{ height: 55, x: 150, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }} className='logo'>
+
+        <div className={hamburger ? ('nav nav-background') : ('nav')}>
+            <motion.div animate={{ height: 55, x: 140, opacity: 1 }} transition={{ ease: "easeOut", duration: 1 }} className='logo'>
             </motion.div>
-            <nav>
-                <ul>
-                    <li>STRONA GŁÓWNA</li>
-                    <li>O FIRMIE</li>
-                    <li>OFERTA</li>
-                    <li>REALIZACJE</li>
-                    <li>KONTAKT</li>
+            <div className={hamburger ? ('menu-container click') : ('menu-container')}>
+                <div onClick={handleClick} className='ham'>
+                    <div className="ham1"></div>
+                    <div className="ham2"></div>
+                    <div className="ham3"></div>
+                </div>
+                <ul className='menu-wrapper'>
+                    <li><Link onClick={handleClick} smooth={true} to="/">STRONA GŁÓWNA</Link></li>
+                    <li><Link onClick={handleClick} smooth={true} to="portfolio">O FIRMIE</Link></li>
+                    <li><Link onClick={handleClick} smooth={true} to="about">OFERTA</Link></li>
+                    <li><Link onClick={handleClick} smooth={true} to="contact">REALIZACJE</Link></li>
+                    <li><Link onClick={handleClick} smooth={true} to="contact">KONTAKT</Link></li>
+
                 </ul>
-            </nav>
-        </div>
+            </div>
+        </div >
     )
 }
 
