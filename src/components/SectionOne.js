355b1e3fa6886ba2ea6f3b457baecd82c165e98a
robkot500 <robert.kotwicki5@gmail.com>
2020-08-30 09:56:56 +0200

    before offer

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -4,7 +4,48 @@ function SectionOne() {
     return (
         <div className='section-one-container'>
             <div className="parallax-one"></div>
-            <div className="offer-wrapper"></div>
+            <div className="offer-container">
+                <div class="offer-header">
+                    <div class="offer-title">Oferta</div>
+                    {/* <ul>
+                        <li>Instalacje elektryczne</li>
+                        <li>Fotowoltaika</li>
+                        <li>Awarie elektryczne</li>
+                        <li>Serwis i naprawa</li>
+                        <li>Pomiary elektryczne</li>
+                    </ul> */}
+                </div>
+                <div class="offer-wrapper">
+                    <div class="offer-wrapper-picture"></div>
+                    <div class="offer-wrapper-right">
+                        <div class="offer-wrapper-name">Instalacje elektryczne</div>
+                        <div class="offer-text">Instalacje Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores minima est aliquam fugiat, sed aperiam nulla doloremque alias eligendi amet eum maxime temporibus, odio mollitia iste hic praesentium laborum architecto enim illo! At esse, temporibus harum voluptatum sit non! Neque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore molestiae, earum, illum doloremque ducimus quidem dolore debitis saepe, excepturi odio voluptatibus! Commodi nostrum delectus accusamus esse aut minima, sunt neque molestias excepturi rerum perspiciatis nihil sapiente perferendis quaerat laudantium velit? </div>
+                        <div class="offer-points">
+                            <div class="offer-points-slogan">Instalacja odbiorcza może składać się z następujących obwodów</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>oświetlenia ogólnego,</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>gniazd wtyczkowych</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>kuchni elektrycznej,</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>elektrycznego podgrzewacza wody,</div>
+                        </div>
+                        <div class="offer-line"></div>
+                    </div>
+                </div>
+                <div class="offer-wrapper">
+                    <div class="offer-wrapper-picture"></div>
+                    <div class="offer-wrapper-right">
+                        <div class="offer-wrapper-name">Instalacje elektryczne</div>
+                        <div class="offer-text">Instalacje Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores minima est aliquam fugiat, sed aperiam nulla doloremque alias eligendi amet eum maxime temporibus, odio mollitia iste hic praesentium laborum architecto enim illo! At esse, temporibus harum voluptatum sit non! Neque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore molestiae, earum, illum doloremque ducimus quidem dolore debitis saepe, excepturi odio voluptatibus! Commodi nostrum delectus accusamus esse aut minima, sunt neque molestias excepturi rerum perspiciatis nihil sapiente perferendis quaerat laudantium velit? </div>
+                        <div class="offer-points">
+                            <div class="offer-points-slogan">Instalacja odbiorcza może składać się z następujących obwodów</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>oświetlenia ogólnego,</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>gniazd wtyczkowych</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>kuchni elektrycznej,</div>
+                            <div><i class="fas fa-long-arrow-alt-right"></i>elektrycznego podgrzewacza wody,</div>
+                        </div>
+                        <div class="offer-line"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
