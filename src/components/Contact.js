e321a66a4be10fd2e899fcc69cbb02d303d1daf0
robkot500 <robert.kotwicki5@gmail.com>
2020-10-13 18:32:01 +0200

    mistake

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,12 +7,18 @@ function Contact() {
     return (
         <Element className='contact-container' name='contact'>
             <div className='contact-title'>Kontakt</div>
-            <div className='contact-slogan'>Skontaktuj się z nami</div>
-            <div className='contact-line'></div>
-            <div className='contact-where'>Działamy na terenie Rybnika i okolic</div>
-            <div className='contact-wrapper'>
-                <div className='contact-tel'><i class="fas fa-phone-alt"></i>692 290 333</div>
-                <div className='contact-email'><i class="fas fa-at"></i>tszoltysek69@gmail.com </div>
+            <div>
+                <div className='contact-line'></div>
+                <div className='contact-wrapper'>
+                    <div className='contact-slogan'>Skontaktuj się z nami</div>
+                    {/* <div className='contact-line'></div> */}
+                    <div className='contact-where'>Działamy na terenie Rybnika i okolic</div>
+                    <div className='contact-tel-email'>
+                        <div className='contact-tel'><i class="fas fa-phone-alt"></i>692 290 333</div>
+                        <div className='contact-email'><i class="fas fa-at"></i>tszoltysek69@gmail.com </div>
+                    </div>
+                </div>
+                <div className='contact-picture'></div>
             </div>
         </Element >
     )
