1e622d74ebb9830a6a33a71fe63db45d4e4a7737
robkot500 <robert.kotwicki5@gmail.com>
2020-10-17 16:38:58 +0200

    next

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,14 +7,14 @@ function Contact() {
     return (
         <Element className='contact-container' name='contact'>
             <div className='contact-title'>Kontakt</div>
-            <div>
-                <div className='contact-line'></div>
+            <div className='contact-box'>
+                {/* <div className='contact-line'></div> */}
                 <div className='contact-wrapper'>
                     <div className='contact-slogan'>Skontaktuj się z nami</div>
-                    {/* <div className='contact-line'></div> */}
-                    <div className='contact-where'>Działamy na terenie Rybnika i okolic</div>
+                    <div className='contact-line'></div>
+                    <div className='contact-where'>Działamy na terenie Rybnika i okolic Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo vitae quidem accusamus eum eius veniam voluptate quasi in suscipit obcaecati?</div>
                     <div className='contact-tel-email'>
-                        <div className='contact-tel'><i class="fas fa-phone-alt"></i>692 290 333</div>
+                        <div className='contact-tel'><i class="fas fa-phone-alt"></i> +48 692 290 333</div>
                         <div className='contact-email'><i class="fas fa-at"></i>tszoltysek69@gmail.com </div>
                     </div>
                 </div>
